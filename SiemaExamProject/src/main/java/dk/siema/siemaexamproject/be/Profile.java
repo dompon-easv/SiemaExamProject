41e1fcc842885aa3d1e0f840b3e7a98749261e39
dominikpon <doominikponik@gmail.com>
2026-04-27 10:06:54 +0200

    added rotation to parameters

--- SiemaExamProject/src/main/java/dk/siema/siemaexamproject/be/Profile.java
+++ SiemaExamProject/src/main/java/dk/siema/siemaexamproject/be/Profile.java
@@ -3,11 +3,13 @@ package dk.siema.siemaexamproject.be;
 public class Profile {
     private int id;
     private String name;
+    private int rotation; //0,90,180,270
     private String splitStrategy;
 
-    public Profile(int id, String name, String splitStrategy) {
+    public Profile(int id, String name,int rotation, String splitStrategy) {
         this.id = id;
         this.name = name;
+        this.rotation = rotation;
         this.splitStrategy = splitStrategy;
     }
 
