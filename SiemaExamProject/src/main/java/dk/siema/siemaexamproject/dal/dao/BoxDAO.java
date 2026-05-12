e8cecf3a45c73ddd59309298e05ad0fd86b82273
dominikpon <doominikponik@gmail.com>
2026-05-12 17:55:33 +0200

    Update BoxDAO.java

--- SiemaExamProject/src/main/java/dk/siema/siemaexamproject/dal/dao/BoxDAO.java
+++ SiemaExamProject/src/main/java/dk/siema/siemaexamproject/dal/dao/BoxDAO.java
@@ -41,23 +41,26 @@ public class BoxDAO implements IBoxDAO {
                     //save document linked to boxId
                     int docId = saveDocument(conn, doc, box.getId());
 
-                    for (FileEntity file : doc.getFiles()) {
-                        //move from staging or insert normally
-                        upsertFile(conn, file, docId);
+                    String insertSql = "INSERT INTO FileEntities (document_id,reference_id, sort_order, rotation, is_barcode) VALUES (?,?,?,?,?)";
+                    try (PreparedStatement filePstmt = conn.prepareStatement(insertSql)) {
+                        for (FileEntity file : doc.getFiles()) {
+                            mapFileParams(filePstmt, file, docId);
+                            filePstmt.addBatch(); //queue the insert
+                        }
+                        filePstmt.executeBatch();
                     }
                 }
                 conn.commit();
             } catch (SQLException e) {
                 conn.rollback();
-                throw new DalException("Transaction failed: " + e.getMessage());
             } finally {
                 conn.setAutoCommit(true);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new DalException("Connection error: " + e.getMessage());
         }
 
-        }
+    }
 
     public void saveBoxMetadata(Connection conn, Box box) throws SQLException {
         // Only insert the INT profile_id
@@ -93,9 +96,9 @@ public class BoxDAO implements IBoxDAO {
                 } else {
                     throw new SQLException("Creating document failed, no ID obtained.");
                 }
-            }}}
-
-
+            }
+        }
+    }
 
 
     private void upsertFile(Connection conn, FileEntity file, int docId) throws SQLException {
@@ -104,7 +107,6 @@ public class BoxDAO implements IBoxDAO {
         //insert metadata into FileEntities
         String insertSql = "INSERT INTO FileEntities " +
                 "(document_id, reference_id, sort_order, rotation, is_barcode) VALUES (?,?,?,?,?)";
-
         byte[] refBytes = BytesConverter.uuidToBytes(file.getReferenceId());
 
         try (PreparedStatement checkPstmt = conn.prepareStatement(checkSql)) {
@@ -113,99 +115,25 @@ public class BoxDAO implements IBoxDAO {
             if (checkPstmt.executeQuery().next()) {
                 //File exists in staging, now save the metadata relatinship
                 try (PreparedStatement insertPstmt = conn.prepareStatement(insertSql)) {
-                    insertPstmt.setInt(1, docId);
-                    insertPstmt.setBytes(2, refBytes);
-                    insertPstmt.setInt(3, file.getSortOrder());
-                    insertPstmt.setInt(4, file.getRotation());
-                    insertPstmt.setBoolean(5, file.isBarcode());
+                    mapFileParams(insertPstmt, file, docId);
                     insertPstmt.executeUpdate();
                 }
             } else
-                insertFileNormally(conn, file, docId);
-
-        }catch (SQLException e){
-            throw new DalException("Upsert failed: " + e.getMessage());
-        }
-        }
-
-    private void insertFileNormally(Connection conn, FileEntity file, int docId) throws SQLException {
-        String sql = "INSERT INTO FileEntities (document_id, reference_id, sort_order, rotation, is_barcode) " +
-                "VALUES (?, ?, ?, ?, ?)";
-
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setInt(1, docId);
-            pstmt.setBytes(2, BytesConverter.uuidToBytes(file.getReferenceId()));
-            pstmt.setInt(3, file.getSortOrder());
-            pstmt.setInt(4, file.getRotation());
-            pstmt.setBoolean(5, file.isBarcode());
-
-            pstmt.executeUpdate();
-        }catch (SQLException e){
+                //not staged ,perform full insert
+               try (PreparedStatement insertPstmt = conn.prepareStatement(insertSql)) {
+                   mapFileParams(insertPstmt, file, docId);
+                   insertPstmt.executeUpdate();
+               }
+        } catch (SQLException e) {
             throw new DalException("Upsert failed: " + e.getMessage());
         }
     }
-
-
-    /*public void saveBox(Box box) throws DalException {
-        String insertBox = "INSERT INTO Boxes (profile_id) VALUES (?)";
-        String insertDoc = "INSERT INTO Documents (box_id) VALUES (?)";
-        String insertFile = "INSERT INTO FileEntities (document_id, reference_id, sort_order, rotation, is_barcode, file_data) VALUES (?, ?, ?, ?, ?, ?)";
-
-        try (Connection conn = ConnectionManager.getConnection()) {
-
-            try {
-                conn.setAutoCommit(false); // START TRANSACTION
-
-                try (PreparedStatement boxStmt = conn.prepareStatement(insertBox, Statement.RETURN_GENERATED_KEYS);
-                     PreparedStatement docStmt = conn.prepareStatement(insertDoc, Statement.RETURN_GENERATED_KEYS);
-                     PreparedStatement fileStmt = conn.prepareStatement(insertFile)) {
-
-                    // 1. Save Box
-                    boxStmt.setString(1, box.getProfileId());
-                    boxStmt.executeUpdate();
-                    box.setId(getGeneratedId(boxStmt));
-
-                    // 2. Save Documents
-                    for (Document doc : box.getDocuments()) {
-                        docStmt.setInt(1, box.getId());
-                        docStmt.executeUpdate();
-                        doc.setId(getGeneratedId(docStmt));
-
-                        // 3. Save Files
-                        for (FileEntity file : doc.getFiles()) {
-                            fileStmt.setInt(1, doc.getId());
-                            fileStmt.setBytes(2, BytesConverter.uuidToBytes(file.getReferenceId()));
-                            fileStmt.setInt(3, file.getSortOrder());
-                            fileStmt.setInt(4, file.getRotation());
-                            fileStmt.setBoolean(5, file.isBarcode());
-                            fileStmt.setBytes(6, file.getFileData());
-                            fileStmt.addBatch(); // add to the bucket instead of sending immediately
-                        }
-                        fileStmt.executeBatch(); //send the whole bucket at once
-                        conn.commit(); // COMMIT TRANSACTION
-                    }
-
-
-                }
-            } catch (SQLException e) {
-                conn.rollback(); // Undo everything if it fails
-                throw new DalException("Database transaction failed while saving the Box hierarchy.", e);
-            } finally {
-                conn.setAutoCommit(true);
-            }
-        } catch (SQLException e) {
-            // Catch connection issues
-            throw new DalException("Could not connect to the database to save the export.", e);
-        }
-    }*/
-
-    private int getGeneratedId(PreparedStatement stmt) throws DalException {
-        try (ResultSet rs = stmt.getGeneratedKeys()) {
-            if (rs.next()) return rs.getInt(1);
-        } catch (SQLException e) {
-            throw new DalException("Failed to retrieve generated ID from database.", e);
-        }
-        throw new DalException("No ID was generated by the database.");
-    }}
-
-
+    private void mapFileParams(PreparedStatement pstmt, FileEntity file, int docId) throws SQLException {
+        byte[] refBytes = BytesConverter.uuidToBytes(file.getReferenceId());
+      pstmt.setInt(1, docId);
+      pstmt.setBytes(2, refBytes);
+      pstmt.setInt(3, file.getSortOrder());
+      pstmt.setInt(4, file.getRotation());
+      pstmt.setBoolean(5, file.isBarcode());
+    }
+}
